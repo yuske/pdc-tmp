1d481373fe629f664dfb072b158d0f9c8286fcda
Mikhail Shcherbakov <yuske.dev@gmail.com>
2023-08-21 13:59:32 +0200

    update

--- 05-mpi/send_recv/send_recv.c
+++ 05-mpi/send_recv/send_recv.c
@@ -9,17 +9,25 @@ int main(int argc, char *argv[] )
   //Add call to mpi init
   //Add call to get rank
   //Add call to get size of communicator
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   //Replace the commented lines with MPI calls
   if (rank == 0) {
     value = 5;
-    printf( "Process %d sending %d\n", rank, value );
+    printf("Process %d sending %d\n", rank, value);
     //Insert MPI command to send value to the next rank
+    MPI_Send(&value, 1, MPI_INT, 1, 7, MPI_COMM_WORLD);
   } else {
     //Insert MPI command to receive value from the previous rank
-    printf( "Process %d got %d\n", rank, value );
-    if (rank < size - 1)
+    int valueRecived;
+    MPI_Recv(&valueRecived, 1, MPI_INT, rank - 1, 7, MPI_COMM_WORLD, &status);
+    printf("Process %d got %d\n", rank, valueRecived);
+    if (rank < size - 1) {
       //Insert MPI command to send value to the next rank
+      MPI_Send(&value, 1, MPI_INT, rank + 1, 7, MPI_COMM_WORLD);
+    }
   }
 
   MPI_Finalize( );
